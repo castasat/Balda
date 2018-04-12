d759c37abd75ac3b75d357751e58357bd47d2c1e
castasat <castasat@mail.ru>
2018-04-12 09:46:50 +0300

    - added LinearLayout(s)Compat inside NestedScrollView(s) in score_fragment.xml to make two lists of words and score for player and his opponent - added score_item.xml with two TextView(s) for word and for it's score (which will be the word's length) - changed res/values/dimens a little bit - in ScoreFragment.java tested creating a list of scrolling items to represent player score or opponent score

--- app/src/main/java/com/openyogaland/denis/balda/ScoreFragment.java
+++ app/src/main/java/com/openyogaland/denis/balda/ScoreFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutCompat.LayoutParams;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ScoreFragment extends Fragment
 {
@@ -15,6 +19,48 @@ public class ScoreFragment extends Fragment
       @Nullable Bundle savedInstanceState)
   {
     View view = inflater.inflate(R.layout.score_fragment, container, false);
+    
+    // TODO just for testing
+    final String WORD = "Балда";
+    final String SCORE = String.valueOf(WORD.length());
+    
+    int colorPlayerScore   = ContextCompat.getColor(getContext(), R.color.colorPlayerScore);
+    int colorOpponentScore = ContextCompat.getColor(getContext(), R.color.colorOpponentScore);
+    
+    for(int i = 0; i < 10; i++)
+    {
+      // player score item
+      LinearLayoutCompat playerScoreLayout = view.findViewById(R.id.playerScoreLayout);
+      View playerItem = inflater.inflate(R.layout.score_item, playerScoreLayout, false);
+      TextView playerWordTextView      = playerItem.findViewById(R.id.wordTextView);
+      TextView playerWordScoreTextView = playerItem.findViewById(R.id.wordScoreTextView);
+      playerWordScoreTextView.setTextColor(colorPlayerScore);
+      
+      // TODO just for testing
+      playerWordTextView.setText(i+1 + ". "+ WORD);
+      playerWordScoreTextView.setText(SCORE);
+  
+      playerItem.getLayoutParams().width = LayoutParams.MATCH_PARENT;
+      playerScoreLayout.addView(playerItem);
+    }
+    
+    for (int i = 0; i < 10; i++)
+    {
+      // opponent score item
+      LinearLayoutCompat opponentScoreLayout = view.findViewById(R.id.opponentScoreLayout);
+      View opponentItem = inflater.inflate(R.layout.score_item, opponentScoreLayout, false);
+      TextView opponentWordTextView = opponentItem.findViewById(R.id.wordTextView);
+      TextView opponentWordScoreTextView = opponentItem.findViewById(R.id.wordScoreTextView);
+      opponentWordScoreTextView.setTextColor(colorOpponentScore);
+  
+      // TODO just for testing
+      opponentWordTextView.setText(i+1 + ". " + WORD);
+      opponentWordScoreTextView.setText(SCORE);
+  
+      opponentItem.getLayoutParams().width = LayoutParams.MATCH_PARENT;
+      opponentScoreLayout.addView(opponentItem);
+    }
+    
     return view;
   }
 }
