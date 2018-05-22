e2745b96a95502cc1b588278858b93c28fc4f7dd
castasat <castasat@mail.ru>
2018-05-22 19:41:35 +0300

    - added int state to Game.java and static final int constants: PLAYER_CHOOSING_LETTER, PLAYER_CHOOSING_WORD and OPPONENT_TURN - removed cell-state-based methods

--- app/src/main/java/com/openyogaland/denis/balda/Game.java
+++ app/src/main/java/com/openyogaland/denis/balda/Game.java
@@ -4,14 +4,10 @@ import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
-import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.widget.Button;
-
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Main activity class Game
@@ -24,15 +20,19 @@ public class Game extends AppCompatActivity implements OnClickListener, OnCellPr
   private static final String TAG_KEYBOARD = "keyboard_fragment_tag";
   private static final String TAG_SCORE    = "score_fragment_tag";
   
+  static final int PLAYER_CHOOSING_LETTER = 0;
+  static final int PLAYER_CHOOSING_WORD   = 1;
+  static final int OPPONENT_TURN          = 2;
+  
   /** alert dialogs **/
   private AlertDialog quitDialog     = null;
   private AlertDialog skipTurnDialog = null;
-  
   /** fragment transaction **/
   private FragmentTransaction transaction;
-  
   /** cell pressed in FieldFragment id **/
   private int cellPressedId;
+  /**  **/
+  private int state = PLAYER_CHOOSING_LETTER;
   
   @Override
   protected void onCreate(Bundle savedInstanceState)
@@ -51,28 +51,119 @@ public class Game extends AppCompatActivity implements OnClickListener, OnCellPr
       transaction.add(R.id.fieldFragmentFrame, fieldFragment, TAG_FIELD);
       transaction.add(R.id.switchableFragmentFrame, scoreFragment, TAG_SCORE);
       transaction.commit();
+      
+      // start player's turn
+      state = PLAYER_CHOOSING_LETTER;
     }
+    
+    // TODO save and restore saved instance state
   }
   
   @Override
-  public boolean onKeyDown(int keyCode, KeyEvent event)
+  public void onKeyPressed(int keyPressedId)
   {
-    if(keyCode == KeyEvent.KEYCODE_BACK)
+    Cell cell = findViewById(cellPressedId);
+    if (cell != null)
     {
-      openQuitDialog();
-      return true;
     }
-    return super.onKeyDown(keyCode, event);
   }
   
-  /**
-   * Take care of popping the fragment back stack or finishing the activity
-   * as appropriate.
-   */
-  @Override
-  public void onBackPressed()
+  private void opponentTurn()
   {
-    openQuitDialog();
+    // TODO
+  }
+  
+  private void playerSkipsHisTurn()
+  {
+    state = OPPONENT_TURN;
+    opponentTurn();
+  }
+  
+  @Override
+  public void onClick(DialogInterface dialogInstance, int which)
+  {
+    // back on phone pressed
+    if((quitDialog != null) && (quitDialog == dialogInstance))
+    {
+      if(which == DialogInterface.BUTTON_POSITIVE)
+      {
+        finish();
+      }
+      else if(which == DialogInterface.BUTTON_NEGATIVE)
+      {
+        dialogInstance.dismiss();
+      }
+    }
+    
+    // back in keyboard fragment pressed
+    if((skipTurnDialog != null) && (skipTurnDialog == dialogInstance))
+    {
+      if(which == DialogInterface.BUTTON_POSITIVE)
+      {
+        dialogInstance.dismiss();
+        playerSkipsHisTurn();
+      }
+      else if(which == DialogInterface.BUTTON_NEGATIVE)
+      {
+        dialogInstance.dismiss();
+      }
+    }
+  }
+  
+  @Override
+  public void onCellPressed(int cellPressedId)
+  {
+    switch(state)
+    {
+      case PLAYER_CHOOSING_LETTER:
+      case PLAYER_CHOOSING_WORD:
+      {
+        this.cellPressedId = cellPressedId;
+        showKeyboard();
+        break;
+      }
+      case OPPONENT_TURN:
+      default:
+      {
+        break;
+      }
+    }
+  }
+  
+  private void showKeyboard()
+  {
+    KeyboardFragment keyboardFragment = (KeyboardFragment) getSupportFragmentManager().findFragmentByTag(TAG_KEYBOARD);
+    
+    if(keyboardFragment == null)
+    {
+      KeyboardFragment fragment = new KeyboardFragment();
+      transaction = getSupportFragmentManager().beginTransaction();
+      transaction.replace(R.id.switchableFragmentFrame, fragment, TAG_KEYBOARD);
+      transaction.commit();
+    }
+  }
+  
+  private void hideKeyboard()
+  {
+    ScoreFragment    scoreFragment    = (ScoreFragment)    getSupportFragmentManager().findFragmentByTag(TAG_SCORE);
+    KeyboardFragment keyboardFragment = (KeyboardFragment) getSupportFragmentManager().findFragmentByTag(TAG_KEYBOARD);
+    
+    transaction = getSupportFragmentManager().beginTransaction();
+    
+    if(scoreFragment != null)
+    {
+      transaction.replace(R.id.switchableFragmentFrame, scoreFragment, TAG_SCORE);
+    }
+    else
+    {
+      scoreFragment = new ScoreFragment();
+      if(keyboardFragment != null)
+      {
+        transaction.remove(keyboardFragment);
+      }
+      transaction.add(R.id.switchableFragmentFrame, scoreFragment, TAG_SCORE);
+    }
+    transaction.commit();
   }
   
   private void openQuitDialog()
@@ -96,233 +187,23 @@ public class Game extends AppCompatActivity implements OnClickListener, OnCellPr
   }
   
   @Override
-  public void onClick(DialogInterface dialogInstance, int which)
+  public boolean onKeyDown(int keyCode, KeyEvent event)
   {
-    // back on phone pressed
-    if ((quitDialog != null) && (quitDialog == dialogInstance))
+    if(keyCode == KeyEvent.KEYCODE_BACK)
     {
-      if(which == DialogInterface.BUTTON_POSITIVE)
-      {
-        finish();
-      }
-      else if(which == DialogInterface.BUTTON_NEGATIVE)
-      {
-        dialogInstance.dismiss();
-      }
+      openQuitDialog();
+      return true;
     }
-    
-    // back in keyboard fragment pressed
-    if ((skipTurnDialog != null) && (skipTurnDialog == dialogInstance))
-    {
-      if(which == DialogInterface.BUTTON_POSITIVE)
-      {
-        dialogInstance.dismiss();
-        // TODO player skips his turn
-      }
-      else if(which == DialogInterface.BUTTON_NEGATIVE)
-      {
-        dialogInstance.dismiss();
-      }
-    }
+    return super.onKeyDown(keyCode, event);
   }
   
+  /**
+   * Take care of popping the fragment back stack or finishing the activity
+   * as appropriate.
+   */
   @Override
-  public void onCellPressed(int cellPressedId)
+  public void onBackPressed()
   {
-    this.cellPressedId = cellPressedId;
-    showKeyboard();
-  }
-  
-  @Override
-  public void onKeyPressed(int keyPressedId)
-  {
-    Cell cell = findViewById(cellPressedId);
-    if (cell != null)
-    {
-      // case cell has no letters
-      keyPressedIfCellHasInitialState(cell, keyPressedId);
-      // case cell has letter choosed state
-      keyPressedIfCellHasLetterChoosedState(cell, keyPressedId);
-      // case cell has letter entered state
-      keyPressedIfCellHasLetterEnteredState(cell, keyPressedId);
-      // case cell has word choosed state
-      keyPressedIfCellHasWordChoosedState(cell, keyPressedId);
-      // case cell has word entered state
-      keyPressedIfCellHasWordEnteredState(cell,keyPressedId);
-    }
-  }
-  
-  private void keyPressedIfCellHasInitialState(@NonNull @NotNull Cell cell, int keyPressedId)
-  {
-    if(cell.getState() == Cell.INITIAL_STATE)
-    {
-      if((keyPressedId == R.id.done) || (keyPressedId == R.id.delete))
-      {
-        // cell has no letters
-        cell.setSelected(false);
-        hideKeyboard();
-        // set state flag
-        cell.setState(Cell.INITIAL_STATE);
-      }
-      else if(keyPressedId == R.id.back)
-      {
-        // ask player if he wants to skip his turn
-        openSkipTurnDialog();
-        cell.setLetter("");
-        cell.setText("");
-        cell.setSelected(false);
-        hideKeyboard();
-        // set state flag
-        cell.setState(Cell.INITIAL_STATE);
-      }
-      else // if letter pressed on keyboard
-      {
-        Button key = findViewById(keyPressedId);
-        if(key != null)
-        {
-          // set the text of the cell equal to the text on the key
-          String letter = key.getText().toString();
-          cell.setText(letter);
-          cell.setLetter(letter);
-          // set state flag
-          cell.setState(Cell.LETTER_CHOOSED);
-        }
-      }
-    }
-  }
-  
-  private void keyPressedIfCellHasLetterChoosedState(@NonNull @NotNull Cell cell, int keyPressedId)
-  {
-    if(cell.getState() == Cell.LETTER_CHOOSED)
-    {
-      if(keyPressedId == R.id.done)
-      {
-        // cell has a letter
-        cell.setSelected(true);
-        cell.setText(cell.getLetter());
-        hideKeyboard();
-        // set state flag
-        cell.setState(Cell.LETTER_ENTERED);
-      }
-      else if(keyPressedId == R.id.back)
-      {
-        // ask player if he wants to skip his turn
-        openSkipTurnDialog();
-        cell.setLetter("");
-        cell.setText("");
-        cell.setSelected(false);
-        hideKeyboard();
-        // set state flag
-        cell.setState(Cell.INITIAL_STATE);
-      }
-      else if(keyPressedId == R.id.delete)
-      {
-        cell.setLetter("");
-        cell.setText("");
-        // set state flag
-        cell.setState(Cell.INITIAL_STATE);
-      }
-      else // if letter pressed on keyboard
-      {
-        // change letter with a new one
-        Button key = findViewById(keyPressedId);
-        if(key != null)
-        {
-          // set the text of the cell equal to the text on the key
-          String letter = key.getText().toString();
-          cell.setText(letter);
-          cell.setLetter(letter);
-          // set state flag
-          cell.setState(Cell.LETTER_CHOOSED);
-        }
-      }
-    }
-  }
-  
-  private void keyPressedIfCellHasLetterEnteredState(@NonNull @NotNull Cell cell, int keyPressedId)
-  {
-    if(cell.getState() == Cell.LETTER_ENTERED)
-    {
-      if(keyPressedId == R.id.done)
-      {
-        // TODO enter the selected word
-      }
-      else if(keyPressedId == R.id.back)
-      {
-        // ask player if he wants to skip his turn
-        openSkipTurnDialog();
-        cell.setLetter("");
-        cell.setText("");
-        cell.setSelected(false);
-        hideKeyboard();
-        // set state flag
-        cell.setState(Cell.INITIAL_STATE);
-      }
-      else if(keyPressedId == R.id.delete)
-      {
-        cell.setLetter("");
-        cell.setText("");
-        // set state flag
-        cell.setState(Cell.INITIAL_STATE);
-      }
-      else // if letter pressed on keyboard
-      {
-        // change letter with a new one
-        Button key = findViewById(keyPressedId);
-        if(key != null)
-        {
-          // set the text of the cell equal to the text on the key
-          String letter = key.getText().toString();
-          cell.setText(letter);
-          cell.setLetter(letter);
-          // set state flag
-          cell.setState(Cell.LETTER_CHOOSED);
-        }
-      }
-    }
-  }
-  
-  private void keyPressedIfCellHasWordChoosedState(@NonNull @NotNull Cell cell, int keyPressedId)
-  {
-  }
-  
-  private void keyPressedIfCellHasWordEnteredState(@NonNull @NotNull Cell cell, int keyPressedId)
-  {
-  }
-  
-  private void showKeyboard()
-  {
-    KeyboardFragment keyboardFragment = (KeyboardFragment) getSupportFragmentManager().findFragmentByTag(TAG_KEYBOARD);
-    
-    if(keyboardFragment == null)
-    {
-      KeyboardFragment fragment = new KeyboardFragment();
-      transaction = getSupportFragmentManager().beginTransaction();
-      transaction.replace(R.id.switchableFragmentFrame, fragment, TAG_KEYBOARD);
-      transaction.commit();
-    }
-  }
-  
-  private void hideKeyboard()
-  {
-    ScoreFragment scoreFragment = (ScoreFragment) getSupportFragmentManager().findFragmentByTag(TAG_SCORE);
-    
-    KeyboardFragment keyboardFragment = (KeyboardFragment) getSupportFragmentManager().findFragmentByTag(TAG_KEYBOARD);
-    
-    transaction = getSupportFragmentManager().beginTransaction();
-    if(scoreFragment != null)
-    {
-      transaction.replace(R.id.switchableFragmentFrame, scoreFragment, TAG_SCORE);
-    }
-    else
-    {
-      scoreFragment = new ScoreFragment();
-      if(keyboardFragment != null)
-      {
-        transaction.remove(keyboardFragment);
-      }
-      transaction.add(R.id.switchableFragmentFrame, scoreFragment, TAG_SCORE);
-    }
-    transaction.commit();
+    openQuitDialog();
   }
 }
